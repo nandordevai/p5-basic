fc0f91fdd913754033473986bbb16dff47b8ce3a
Nandor Devai <mail@nandordevai.net>
2020-03-11 14:03:18 +0100

    add recursive boxes

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 function createControls(controls) {
     controls.forEach((c, i) => {
         const s = createSlider(c.min, c.max, c.default);
-        s.position(100, 200 + i * 30);
+        s.position(120, 200 + i * 30);
         const l = createDiv(c.label);
         l.class('label');
-        l.position(40, 198 + i * 30);
+        l.position(50, 198 + i * 30);
         s.input(() => {
             if (c.callback) {
                 c.callback(s);
