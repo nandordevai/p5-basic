d7de2ee24097e032577751b947aedc3c72c3d090
Nandor Devai <mail@nandordevai.net>
2020-03-19 18:43:12 +0100

    use createControls()

--- lines-repeated-smooth/sketch.js
+++ lines-repeated-smooth/sketch.js
@@ -32,19 +32,7 @@ const controls = [
 function setup() {
     createCanvas(size, size);
     noFill();
-    controls.forEach((c, i) => {
-        const s = createSlider(c.min, c.max, c.default);
-        s.position(100, 200 + i * 30);
-        const l = createDiv(c.label);
-        l.class('label');
-        l.position(50, 198 + i * 30);
-        s.input(() => {
-            if (c.callback) {
-                c.callback(s);
-            }
-            draw();
-        });
-    });
+    createControls(controls);
 }
 
 function drawLine(y) {
