6ec252c8fb9619c699c3d42f8eb4836b6b376dbb
Nandor Devai <mail@nandordevai.net>
2020-03-20 09:41:23 +0100

    noisefield refining

--- noisefield/sketch.js
+++ noisefield/sketch.js
@@ -2,13 +2,21 @@ const size = 400;
 const step = 10;
 const field = [];
 let force = 50;
+let sat = 50;
 const controls = [
     {
         min: 20,
-        max: 100,
+        max: 500,
         label: 'Force',
         default: force,
         callback: (_) => force = _.value(),
+    },
+    {
+        min: 20,
+        max: 100,
+        label: 'Saturation',
+        default: sat,
+        callback: (_) => sat = _.value(),
     },
 ];
 
@@ -18,17 +26,14 @@ function setup() {
     noFill();
 }
 
-const field_ = [
-    [1, 2, 3], // row, y constant
-]
-
 function draw() {
-    background(255);
+    background(0);
+    strokeWeight(4);
     noLoop();
     for (let y = 0; y < step; y++) {
         field.push([]);
         for (let x = 0; x < step; x++) {
-            field[y][x] = (noise(x / 10, y / 10) - 0.5) * force;
+            field[y][x] = noise(x / 10, y / 10);
         }
     }
     const l = size / step;
@@ -37,15 +42,17 @@ function draw() {
     colorMode(HSL);
     translate(-l, 0);
     for (let x = 0; x < step; x++) {
-        stroke(x * 10, 55, 70);
+        stroke(x * 10, sat, 70);
         beginShape();
         translate(l, 0);
         let pos = l / 2;
+        curveVertex(pos, 0);
         for (let y = 0; y < step; y++) {
-            vertex(pos, y * l);
-            pos -= field[x][y];
+            curveVertex(pos, y * l);
+            pos -= (field[x][y] - 0.5) * force;
         }
-        vertex(pos, size);
+        curveVertex(pos, size);
+        curveVertex(pos, size);
         endShape();
     }
     pop();
